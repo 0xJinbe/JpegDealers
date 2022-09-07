5d9b90e31b047b5450eecca9a67944418d18e3f2
claudio-germano <wantuir.germano@gmail.com>
2022-09-07 10:01:07 -0300

    bootloader sound volume increase

--- js/jpeg-dealers-retro-desktop.js
+++ js/jpeg-dealers-retro-desktop.js
@@ -1,10 +1,4 @@
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
+
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
